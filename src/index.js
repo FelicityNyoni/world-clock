032124fcfaae419cfb25eedd6a2be542851881da
FelicityNyoni <felinyoni29@gmail.com>
2024-11-24 21:51:15 +0200

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function updateCity(event) {
         </div>
         <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small</div>
          </div>
-         <a href ="index.html"></a>`;
+         <a href ="/"></a>`;
 }
 }
 updateTime();
