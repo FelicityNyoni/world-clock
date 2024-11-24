4a11a47c1a1c1837685d7b4f7b6c8c27a6db1e85
FelicityNyoni <felinyoni29@gmail.com>
2024-11-24 22:42:53 +0200

    Updated the link

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ function updateCity(event) {
         </div>
         <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small</div>
          </div>
-         <a href="/"></a>`;
+         <a href="/">All cities</a>`;
 }
 }
 updateTime();
